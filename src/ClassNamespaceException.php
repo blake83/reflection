48e0430c7c9c18c36bfe98d7efc8b59a1213ff8b
Jakub Giminski <jakub@gigabyte.software>
2019-03-18 23:09:50 +0000

    Fix exception method, test basic validation

--- src/ClassNamespaceException.php
+++ src/ClassNamespaceException.php
@@ -6,9 +6,9 @@ use RuntimeException;
 
 class ClassNamespaceException extends RuntimeException
 {
-    public static function invalidNamespace(string $class): self
+    public static function invalidNamespace(string $namespace): self
     {
-
+        return new self("Provided namespace $namespace is not a valid class");
     }
 
     public static function missingInterface(ClassNamespace $class, ClassNamespace $interface): self
--- test/ClassNamespaceTest.php
+++ test/ClassNamespaceTest.php
@@ -3,7 +3,9 @@
 namespace Comquer\ReflectionTest;
 
 use Comquer\Reflection\ClassNamespace;
+use Comquer\Reflection\ClassNamespaceException;
 use Comquer\ReflectionTest\Fixture\SampleClass;
+use Comquer\ReflectionTest\Fixture\SampleInterface;
 use PHPUnit\Framework\TestCase;
 
 class ClassNamespaceTest extends TestCase
@@ -15,5 +17,15 @@ class ClassNamespaceTest extends TestCase
             ClassNamespace::class,
             new ClassNamespace(SampleClass::class)
         );
+    }
+
+    /** @test */
+    function cant_instantiate_with_interface_namespace()
+    {
+        $exception = ClassNamespaceException::invalidNamespace(SampleInterface::class);
+        $this->expectException(get_class($exception));
+        $this->expectExceptionMessage($exception->getMessage());
+
+        new ClassNamespace(SampleInterface::class);
     }
 }
